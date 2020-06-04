e10eb724374229536c29caceca50a87f0eec5b9d
tsn778 <tsn778@126.com>
2020-06-04 11:05:39 +0800

    注册与登录

---
+++ test1/Controllers/LoginController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace test1.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Index2()
+        {
+            return View();
+        }
+    }
+}
